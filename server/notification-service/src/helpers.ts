43158121bd10bafd3aebb02d9a3ea5bf9fca90c3
nhkhangnek <khanggg061203@gmail.com>
2024-10-26 07:31:01 +0700

    notification-service: email templates

---
+++ server/notification-service/src/helpers.ts
@@ -0,0 +1,53 @@
+import { config } from "@notification/config"
+import { IEmailLocals, winstonLogger } from "@tanlan/jobber-shared"
+import { Logger } from "winston"
+import nodemailer, { Transporter } from 'nodemailer'
+import Email from "email-templates"
+import path from 'path'
+
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'mailTransport', 'debug')
+
+async function emailTemplates(template: string, reciver: string, locals: IEmailLocals): Promise<void> {
+    try {
+        const smtpTransporter: Transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: config.SENDER_EMAIL,
+                pass: config.SENDER_EMAIL_PASSWORD
+            }
+        })
+
+        const email: Email = new Email({
+            message: {
+                from: `Jobber App <${config.SENDER_EMAIL}>`
+            },
+            send: true, 
+            preview: false,
+            transport: smtpTransporter,
+            views: {
+                options: {
+                    extension: 'ejs'
+                }
+            }, 
+            juice: true,
+            juiceResources: {
+                preserveImportant: true,
+                webResources: {
+                    rebaseRelativeTo: path.join(__dirname, '../build')
+                }
+            }
+        })
+
+        await email.send({
+            template: path.join(__dirname,  '..', 'src/emails', template),
+            message: {to: reciver},
+            locals
+        })
+    } catch (error) {
+        log.error(error)
+    }
+}
+
+export { emailTemplates }
--- server/notification-service/src/queues/mail.transport.ts
+++ server/notification-service/src/queues/mail.transport.ts
@@ -1,12 +1,14 @@
 import { config } from "@notification/config"
+import { emailTemplates } from "@notification/helpers"
 import { IEmailLocals, winstonLogger } from "@tanlan/jobber-shared"
 import { Logger } from "winston"
 
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'mailTransport', 'debug')
 
-async function sendEmail(template:string, reciver: string, local: IEmailLocals): Promise<void> {
+async function sendEmail(template:string, reciver: string, locals: IEmailLocals): Promise<void> {
     try {
         //logic send email
+        emailTemplates(template, reciver, locals)
         log.info('Email send success')
     } catch (error) {
         log.log('error', 'NotificationService sendEmail() method:', error);
