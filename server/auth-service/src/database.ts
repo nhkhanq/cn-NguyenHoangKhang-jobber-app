6c89f826c24e48934b642c7d2658e697fe6cceb6
NHKhanq <nhkhang061203@gmail.com>
2024-11-04 19:57:12 +0700

    auth-service: setup connnect elasticsearch

--- server/auth-service/src/database.ts
+++ server/auth-service/src/database.ts
@@ -6,7 +6,8 @@ import { Sequelize } from 'sequelize'
 
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'authDatabase', 'debug')
 
-export const sequelize = new Sequelize({
+
+export const sequelize = new Sequelize(process.env.MYSQL_DB || '' ,{
     dialect: 'mysql',
     logging: false,
     dialectOptions: {
---
+++ server/auth-service/src/elasticsearch.ts
@@ -0,0 +1,25 @@
+import { Client } from '@elastic/elasticsearch'
+import { ClusterHealthResponse } from '@elastic/elasticsearch/lib/api/types'
+import { config } from '@auth/config'
+import { winstonLogger } from '@tanlan/jobber-shared'
+import { Logger } from 'winston'
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'authElasticSearchServer', 'debug')
+
+const elasticsearchClient = new Client({
+    node: `${config.ELASTIC_SEARCH_URL}`
+})
+
+export async function checkConnection(): Promise<void> {
+    let isConnected = false;
+    while (!isConnected) {
+      try {
+        const health: ClusterHealthResponse = await elasticsearchClient.cluster.health({})
+        log.info(`AuthService Elasticsearch health status - ${health.status}`)
+        isConnected = true
+      } catch (error) {
+        log.error('Connection to Elasticsearch failed')
+        log.log('error', 'AuthService checkConnection() method:', error)
+      }
+    }
+  }
