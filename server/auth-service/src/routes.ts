503cb7f7d512be7db26cbdb737017dc8512d385c
NHKhanq <nhkhang061203@gmail.com>
2024-11-18 22:02:11 +0700

    auth-service: signup route

--- server/auth-service/src/routes.ts
+++ server/auth-service/src/routes.ts
@@ -1,7 +1,8 @@
 import { Application } from "express";
+import { authRoutes } from "@auth/routes/auth";
+import { verifyGatewayRequest } from "@tanlan/jobber-shared";
 
-
+const BASE_URL = 'api/v1/auth'
 export function appRoutes(app: Application): void {
-    app.use('', () => {console.log("test");
-    })
+    app.use(BASE_URL, verifyGatewayRequest, authRoutes())
 }
--- server/gateway-service/src/server.ts
+++ server/gateway-service/src/server.ts
@@ -1,16 +1,20 @@
-import { winstonLogger, IErrorResponse, CustomError } from '@tanlan/jobber-shared'
-import { Application, json, NextFunction, urlencoded, Request, Response } from 'express'
-import { error, Logger } from 'winston'
+import http from 'http'
+
+import 'express-async-errors'
+import { CustomError, IErrorResponse, winstonLogger } from '@tanlan/jobber-shared'
+import { Application, Request, Response, json, urlencoded, NextFunction } from 'express'
+import { Logger } from 'winston'
 import cookieSession from 'cookie-session'
 import cors from 'cors'
 import hpp from 'hpp'
 import helmet from 'helmet'
 import compression from 'compression'
 import { StatusCodes } from 'http-status-codes'
-import http from 'http'
 import { config } from '@gateway/config'
 import { elasticSearch } from '@gateway/elasticsearch'
 import { appRoutes } from '@gateway/routes'
+import { axiosAuthInstance } from '@gateway/services/api/auth.service'
+import { isAxiosError } from 'axios'
 
 const SERVER_PORT = 4000
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug')
@@ -49,57 +53,63 @@ export class GatewayServer {
             credentials: true,
             methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
         }))
-    }
 
-    private standarMiddleware(app: Application): void {
-        app.use(compression())
-        app.use(json({ limit: '200mb' }))
-        app.use(urlencoded({ extended: true, limit: '200mb' }))
-    }
+    app.use((req: Request, _res: Response, next: NextFunction) => {
+      if (req.session?.jwt) {
+        axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`
+      }
+      next()
+    })
+  }
 
-    private routeMiddleware(app: Application): void {
-        appRoutes(app)
-    }
-
-    private startElasticSearch(app: Application): void {
-        elasticSearch.checkConnection()
-    }
-
-    private errorHandler(app: Application): void {
-        app.use('*', (req: Request, res: Response, next: NextFunction) => {
-            const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
-            log.error(`${fullUrl} endpoint does not exist`)
-            res.status(StatusCodes.NOT_FOUND).json('Endpoint does not exist')
-            next()
-        })
-
-        app.use((error: IErrorResponse, req: Request, res: Response, next: NextFunction) => {
-            log.log('error',`Gateway Service ${error.comingFrom}:`, error)
-            if(error instanceof CustomError) {
-                res.status(error.statusCode).json('Endpoint does not exist')
-            }
-            next()
-        })
-    }
-
-    private async startServer(app: Application): Promise<void> {
-        try {
-            const httpServer: http.Server  = new http.Server(app)
-            this.startHttpServer(httpServer)
-        } catch (error) {
-        log.log('error', 'Gateway Service startServer() method:', error)
-        }
-    }
-
-    private async startHttpServer(httpServer: http.Server): Promise<void> {
-        try {
-            httpServer.listen(SERVER_PORT, () => {
-                log.log('info', `Server is running on port ${SERVER_PORT}`)
-            })   
-        } catch (error) {
-            log.log('error', 'Gateway Service startServer() method:', error)
-        }
-    }
-    
+  private standarMiddleware(app: Application): void {
+    app.use(compression())
+    app.use(json({ limit: '200mb' }))
+    app.use(urlencoded({ extended: true, limit: '200mb' }))
 }
 
+private routeMiddleware(app: Application): void {
+    appRoutes(app)
+}
+
+private startElasticSearch(app: Application): void {
+    elasticSearch.checkConnection()
+}
+
+private errorHandler(app: Application): void {
+    app.use('*', (req: Request, res: Response, next: NextFunction) => {
+        const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
+        log.error(`${fullUrl} endpoint does not exist`)
+        res.status(StatusCodes.NOT_FOUND).json('Endpoint does not exist')
+        next()
+    })
+
+    app.use((error: IErrorResponse, req: Request, res: Response, next: NextFunction) => {
+        log.log('error',`Gateway Service ${error.comingFrom}:`, error)
+        if(error instanceof CustomError) {
+            res.status(error.statusCode).json('Endpoint does not exist')
+        }
+        next()
+    })
+}
+
+private async startServer(app: Application): Promise<void> {
+    try {
+        const httpServer: http.Server  = new http.Server(app)
+        this.startHttpServer(httpServer)
+    } catch (error) {
+    log.log('error', 'Gateway Service startServer() method:', error)
+    }
+}
+
+private async startHttpServer(httpServer: http.Server): Promise<void> {
+    try {
+        httpServer.listen(SERVER_PORT, () => {
+            log.log('info', `Server is running on port ${SERVER_PORT}`)
+        })   
+    } catch (error) {
+        log.log('error', 'Gateway Service startServer() method:', error)
+    }
+}
+
+}
