38dd844ef70b6d6ce43a150af71e870083614705
NHKhanq <nhkhang061203@gmail.com>
2024-11-25 16:15:11 +0700

    auth-service-elastic: search route

--- server/gateway-service/src/controllers/current-user.ts
+++ server/gateway-service/src/controllers/current-user.ts
@@ -3,10 +3,14 @@ import { AxiosResponse } from 'axios';
 import { authService } from '@gateway/services/api/auth.service';
 import { StatusCodes } from 'http-status-codes';
 
-export class SignUp {
+export class CurrentUser {
   public async read(req: Request, res: Response) {
-    const response: AxiosResponse = await authService.getCurrentUser();
-    req.session = { jwt: response.data.token }; 
+    const response: AxiosResponse = await authService.getCurrentUser() 
+    res.status(StatusCodes.CREATED).json({ message: response.data.message, user: response.data.user });
+  }
+
+  public async resendEmail(req: Request, res: Response) {
+    const response: AxiosResponse = await authService.resendEmail(req.body) 
     res.status(StatusCodes.CREATED).json({ message: response.data.message, user: response.data.user });
   }
 }
