927d11e70c971e925d598a861e9f4dee59d8f5c3
NHKhanq <nhkhang061203@gmail.com>
2024-12-03 15:54:09 +0700

    users-service: seller get controller

--- server/users-service/src/routes/seller.ts
+++ server/users-service/src/routes/seller.ts
@@ -1,10 +1,14 @@
 import { seller as createSeller } from '@users/controllers/seller/create'
+import { id, random, username } from '@users/controllers/seller/get'
 import { seller as updateSeller } from '@users/controllers/seller/update'
 import express, { Router } from 'express'
 
 const router: Router = express.Router()
 
 const sellerRoutes = (): Router => {
+  router.get('/id/:sellerId', id)
+  router.get('/username/:username', username)
+  router.get('/random/:size', random);
   router.post('/create', createSeller)
   router.put('/:sellerId', updateSeller)
   return router
