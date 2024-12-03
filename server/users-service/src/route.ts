0b9a86bbe6813df805feb33f18065285070d6224
NHKhanq <nhkhang061203@gmail.com>
2024-12-03 14:47:51 +0700

    users-service: buyer controller routes

--- server/users-service/src/route.ts
+++ server/users-service/src/route.ts
@@ -1,12 +1,14 @@
 import { Application } from 'express'
 import { verifyGatewayRequest } from 'jobber-shared-for-hkhanq'
+import { buyerRoutes } from '@users/routes/buyer'
+import { healthRoutes } from './routes/health'
 
 const BUYER_BASE_PATH = '/api/v1/buyer'
 const SELLER_BASE_PATH = '/api/v1/seller'
 
 const appRoutes = (app: Application): void => {
-  app.use('', () => {})
-  app.use(BUYER_BASE_PATH, verifyGatewayRequest, () => {})
+  app.use('', healthRoutes())
+  app.use(BUYER_BASE_PATH, verifyGatewayRequest, buyerRoutes())
   app.use(SELLER_BASE_PATH, verifyGatewayRequest, () => {})
 }
 
