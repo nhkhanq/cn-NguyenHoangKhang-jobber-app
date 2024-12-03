aa2c4a65ce49aec89c6f2409a72beab1c5a76a3e
NHKhanq <nhkhang061203@gmail.com>
2024-12-03 13:29:13 +0700

    users-service: rabbitmq review message consumer method

--- server/users-service/src/queues/user.consumer.ts
+++ server/users-service/src/queues/user.consumer.ts
@@ -1,10 +1,11 @@
 import { config } from '@users/config'
 import { createConnection } from '@users/queues/connection'
 import { createBuyer, updateBuyerPurchasedGigsProp } from '@users/service/buyer.service'
-import { IBuyerDocument, winstonLogger } from 'jobber-shared-for-hkhanq'
+import { IBuyerDocument, ISellerDocument, winstonLogger } from 'jobber-shared-for-hkhanq'
 import { Channel, ConsumeMessage, Replies } from 'amqplib'
 import { Logger } from 'winston'
-import { updateSellerCancelledJobsProp, updateSellerCompletedJobsProp, updateSellerOngoingJobsProp, updateTotalGigsCount } from '@users/service/seller.service'
+import { getRandomSellers, updateSellerCancelledJobsProp, updateSellerCompletedJobsProp, updateSellerOngoingJobsProp, updateSellerReview, updateTotalGigsCount } from '@users/service/seller.service'
+import { publishDirectMessage } from './user.producer'
 
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'usersServiceConsumer', 'debug')
 
@@ -80,5 +81,65 @@ const consumeSellerDirectMessage = async (channel: Channel): Promise<void> => {
     }
   }
 
+  const consumeReviewFanoutMessages = async (channel: Channel): Promise<void> => {
+    try {
+      if (!channel) {
+        channel = (await createConnection()) as Channel
+      }
+      const exchangeName = 'jobber-review'
+      const queueName = 'seller-review-queue'
+      await channel.assertExchange(exchangeName, 'fanout')
+      const jobberQueue: Replies.AssertQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false })
+      await channel.bindQueue(jobberQueue.queue, exchangeName, '')
+      channel.consume(jobberQueue.queue, async (msg: ConsumeMessage | null) => {
+        const { type } = JSON.parse(msg!.content.toString())
+        if (type === 'buyer-review') {
+          await updateSellerReview(JSON.parse(msg!.content.toString()))
+          await publishDirectMessage(
+            channel,
+            'jobber-update-gig',
+            'update-gig',
+            JSON.stringify({ type: 'updateGig', gigReview: msg!.content.toString() }),
+            'Message sent to gig service.'
+          )
+        }
+        channel.ack(msg!)
+      })
+    } catch (error) {
+      log.log('error', 'UsersService UserConsumer consumeReviewFanoutMessages() method error:', error)
+    }
+  }
+  
+  const consumeSeedGigDirectMessages = async (channel: Channel): Promise<void> => {
+    try {
+      if (!channel) {
+        channel = (await createConnection()) as Channel
+      }
+      const exchangeName = 'jobber-gig'
+      const routingKey = 'get-sellers'
+      const queueName = 'user-gig-queue'
+      await channel.assertExchange(exchangeName, 'direct')
+      const jobberQueue: Replies.AssertQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false })
+      await channel.bindQueue(jobberQueue.queue, exchangeName, routingKey)
+      channel.consume(jobberQueue.queue, async (msg: ConsumeMessage | null) => {
+        const { type } = JSON.parse(msg!.content.toString())
+        if (type === 'getSellers') {
+          const { count } = JSON.parse(msg!.content.toString())
+          const sellers: ISellerDocument[] = await getRandomSellers(parseInt(count, 10))
+          await publishDirectMessage(
+            channel,
+            'jobber-seed-gig',
+            'receive-sellers',
+            JSON.stringify({ type: 'receiveSellers', sellers, count }),
+            'Message sent to gig service.'
+          )
+        }
+        channel.ack(msg!)
+      })
+    } catch (error) {
+      log.log('error', 'UsersService UserConsumer consumeReviewFanoutMessages() method error:', error)
+    }
+  }
 
-export { consumeBuyerDirectMessage, consumeSellerDirectMessage }
+
+export { consumeBuyerDirectMessage, consumeSellerDirectMessage, consumeReviewFanoutMessages, consumeSeedGigDirectMessages }
