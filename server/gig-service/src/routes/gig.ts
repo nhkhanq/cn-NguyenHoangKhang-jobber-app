150a0f49cff1b51d5269b8fc680ef9044e3d7634
NHKhanq <nhkhang061203@gmail.com>
2024-12-08 12:44:43 +0700

    gigs-service: update, delete controller method

--- server/gig-service/src/routes/gig.ts
+++ server/gig-service/src/routes/gig.ts
@@ -1,4 +1,6 @@
 import { gigCreate } from '@gig/controller/create'
+import { gigDelete } from '@gig/controller/delete'
+import { gigUpdate, gigUpdateActive } from '@gig/controller/update'
 import express, { Router } from 'express'
 
 const router: Router = express.Router()
@@ -6,6 +8,9 @@ const router: Router = express.Router()
 
 const gigRoutes = (): Router => {
   router.post('/create', gigCreate)
+  router.put('/:gigId', gigUpdate)
+  router.put('/active/:gigId', gigUpdateActive)
+  router.delete('/:gigId/:sellerId', gigDelete)
   return router
 }
 
