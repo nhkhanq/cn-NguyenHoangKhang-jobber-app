ccf68b19bf5fc508080294fef30e00a017ad5f88
NHKhanq <nhkhang061203@gmail.com>
2024-12-08 15:04:48 +0700

    gigs-service: search controller

--- server/gig-service/src/controller/get.ts
+++ server/gig-service/src/controller/get.ts
@@ -2,7 +2,9 @@
 import { getGigById, getSellerGigs, getSellerPausedGigs } from '@gig/services/gig.service'
 import { ISearchResult, ISellerGig } from 'jobber-shared-for-hkhanq'
 import { Request, Response } from 'express'
+import { getUserSelectedGigCategory } from '@gig/redis/redis.cache'
 import { StatusCodes } from 'http-status-codes'
+import { getMoreGigsLikeThis, getTopRatedGigsByCategory, gigsSearchByCategory } from '@gig/services/search.service'
 
 const gigById = async (req: Request, res: Response): Promise<void> => {
   const gig: ISellerGig = await getGigById(req.params.gigId)
@@ -19,7 +21,34 @@ const sellerInactiveGigs = async (req: Request, res: Response): Promise<void> =>
   res.status(StatusCodes.OK).json({ message: 'Seller gigs', gigs })
 }
 
+const topRatedGigsByCategory = async (req: Request, res: Response): Promise<void> => {
+    const category = await getUserSelectedGigCategory(`selectedCategories:${req.params.username}`)
+    const resultHits: ISellerGig[] = []
+    const gigs: ISearchResult = await getTopRatedGigsByCategory(`${category}`)
+    for(const item of gigs.hits) {
+      resultHits.push(item._source as ISellerGig)
+    }
+    res.status(StatusCodes.OK).json({ message: 'Search top gigs results', total: gigs.total, gigs: resultHits })
+  }
+  
+  const gigsByCategory = async (req: Request, res: Response): Promise<void> => {
+    const category = await getUserSelectedGigCategory(`selectedCategories:${req.params.username}`)
+    const resultHits: ISellerGig[] = []
+    const gigs: ISearchResult = await gigsSearchByCategory(`${category}`)
+    for(const item of gigs.hits) {
+      resultHits.push(item._source as ISellerGig)
+    }
+    res.status(StatusCodes.OK).json({ message: 'Search gigs category results', total: gigs.total, gigs: resultHits })
+  }
+  
+  const moreLikeThis = async (req: Request, res: Response): Promise<void> => {
+    const resultHits: ISellerGig[] = []
+    const gigs: ISearchResult = await getMoreGigsLikeThis(req.params.gigId)
+    for(const item of gigs.hits) {
+      resultHits.push(item._source as ISellerGig)
+    }
+    res.status(StatusCodes.OK).json({ message: 'More gigs like this result', total: gigs.total, gigs: resultHits })
+  }
 
 
-
-export { gigById, sellerGigs, sellerInactiveGigs }
+export { gigById, sellerGigs, sellerInactiveGigs, topRatedGigsByCategory, gigsByCategory, moreLikeThis }
--- server/gig-service/src/routes/gig.ts
+++ server/gig-service/src/routes/gig.ts
@@ -1,6 +1,7 @@
 import { gigCreate } from '@gig/controller/create'
 import { gigDelete } from '@gig/controller/delete'
-import { gigById, sellerGigs, sellerInactiveGigs } from '@gig/controller/get'
+import { gigById, gigsByCategory, moreLikeThis, sellerGigs, sellerInactiveGigs, topRatedGigsByCategory } from '@gig/controller/get'
+import { gigs } from '@gig/controller/search'
 import { gigUpdate, gigUpdateActive } from '@gig/controller/update'
 import express, { Router } from 'express'
 
@@ -11,6 +12,10 @@ const gigRoutes = (): Router => {
   router.get('/:gigId', gigById)
   router.get('/seller/:sellerId', sellerGigs)
   router.get('/seller/pause/:sellerId', sellerInactiveGigs)
+  router.get('/search/:from/:size/:type', gigs)
+  router.get('/category/:username', gigsByCategory);
+  router.get('/top/:username', topRatedGigsByCategory);
+  router.get('/similar/:gigId', moreLikeThis);
   router.post('/create', gigCreate)
   router.put('/:gigId', gigUpdate)
   router.put('/active/:gigId', gigUpdateActive)
