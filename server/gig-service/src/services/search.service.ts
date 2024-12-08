4af9704280b7ce54a16c2dcae34d5f48b5d1d1e7
NHKhanq <nhkhang061203@gmail.com>
2024-12-08 14:14:30 +0700

    gigs-service: elaticsearch more gig like this method

--- server/gig-service/src/services/search.service.ts
+++ server/gig-service/src/services/search.service.ts
@@ -31,4 +31,119 @@ const gigsSearchBySellerId = async (searchQuery: string, active: boolean): Promi
   }
 }
 
+const gigsSearch = async (
+  searchQuery: string,
+  paginate: IPaginateProps,
+  deliveryTime?: string,
+  min?: number,
+  max?: number
+): Promise<ISearchResult> => {
+  const { from, size, type } = paginate
+  const queryList: IQueryList[] = [
+    {
+      query_string: {
+        fields: ['username', 'title', 'description', 'basicDescription', 'basicTitle', 'categories', 'subCategories', 'tags'],
+        query: `*${searchQuery}*`
+      }
+    },
+    {
+      term: {
+        active: true
+      }
+    }
+  ]
+
+  if (deliveryTime !== 'undefined') {
+    queryList.push({
+      query_string: {
+        fields: ['expectedDelivery'],
+        query: `*${deliveryTime}*`
+      }
+    })
+  }
+
+  if (!isNaN(parseInt(`${min}`)) && !isNaN(parseInt(`${max}`))) {
+    queryList.push({
+      range: {
+        price: {
+          gte: min,
+          lte: max
+        }
+      }
+    })
+  }
+  const result: SearchResponse = await elasticSearchClient.search({
+    index: 'gigs',
+    size,
+    query: {
+      bool: {
+        must: [...queryList]
+      }
+    },
+    sort: [
+      {
+        sortId: type === 'forward' ? 'asc' : 'desc'
+      }
+    ],
+    ...(from !== '0' && { search_after: [from] })
+  })
+  const total: IHitsTotal = result.hits.total as IHitsTotal
+  return {
+    total: total.value,
+    hits: result.hits.hits
+  }
+}
+
+const gigsSearchByCategory = async (searchQuery: string): Promise<ISearchResult> => {
+  const result: SearchResponse = await elasticSearchClient.search({
+    index: 'gigs',
+    size: 10,
+    query: {
+      bool: {
+        must: [
+          {
+            query_string: {
+              fields: ['categories'],
+              query: `*${searchQuery}*`
+            }
+          },
+          {
+            term: {
+              active: true
+            }
+          }
+        ]
+      }
+    },
+  })
+  const total: IHitsTotal = result.hits.total as IHitsTotal
+  return {
+    total: total.value,
+    hits: result.hits.hits
+  }
+}
+
+const getMoreGigsLikeThis = async (gigId: string): Promise<ISearchResult> => {
+  const result: SearchResponse = await elasticSearchClient.search({
+    index: 'gigs',
+    size: 5,
+    query: {
+      more_like_this: {
+        fields: ['username', 'title', 'description', 'basicDescription', 'basicTitle', 'categories', 'subCategories', 'tags'],
+        like: [
+          {
+            _index: 'gigs',
+            _id: gigId
+          }
+        ]
+      }
+    }
+  })
+  const total: IHitsTotal = result.hits.total as IHitsTotal
+  return {
+    total: total.value,
+    hits: result.hits.hits
+  }
+}
+
 export { gigsSearchBySellerId }
