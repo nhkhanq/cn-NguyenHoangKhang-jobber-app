eb52380831da3dcdbf34446fd9af5a5095a03b05
NHKhanq <nhkhang061203@gmail.com>
2024-12-10 15:18:53 +0700

    gigs-service: update current user controller

--- server/gateway-service/src/routes/current-user.ts
+++ server/gateway-service/src/routes/current-user.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express'
-import { CurrentUser } from '@gateway/controllers/current-user'
+import { CurrentUser } from '@gateway/controllers/auth/current-user'
 import { authMiddleware } from '@gateway/services/auth-middleware'
+import { Refresh } from '@gateway/controllers/auth/refresh-token'
 
 class CurrentUserRoutes {
     private router: Router
@@ -10,10 +11,13 @@ class CurrentUserRoutes {
     }
 
     public routes(): Router {
-        this.router.get('/auth/currentuser', authMiddleware.checkAuthentication, CurrentUser.prototype.read);
-        this.router.post('/auth/resend-email', authMiddleware.checkAuthentication, CurrentUser.prototype.resendEmail);
+        this.router.get('/auth/refresh-token/:username', authMiddleware.checkAuthentication, Refresh.prototype.token)
+        this.router.get('/auth/logged-in-user', authMiddleware.checkAuthentication, CurrentUser.prototype.getLoggedInUsers)
+        this.router.get('/auth/currentuser', authMiddleware.checkAuthentication, CurrentUser.prototype.read)
+        this.router.post('/auth/resend-email', authMiddleware.checkAuthentication, CurrentUser.prototype.resendEmail)
+        this.router.delete('/auth/logged-in-user/:username', authMiddleware.checkAuthentication, CurrentUser.prototype.removeLoggedInUser)
         return this.router
-    }
+      }
 }
 
 export const currentUserRoutes: CurrentUserRoutes = new CurrentUserRoutes()
