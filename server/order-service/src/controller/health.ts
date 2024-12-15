51d6471cab126446d22d95f79a0e53bc3e7d5ddd
NHKhanq <nhkhang061203@gmail.com>
2024-12-15 08:12:16 +0700

    order-service: order model

--- server/order-service/src/controller/health.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express'
-import { StatusCodes } from 'http-status-codes'
-
-const health = (_req: Request, res: Response): void => {
-  res.status(StatusCodes.OK).send('Chat service is healthy')
-}
-
-export { health }
