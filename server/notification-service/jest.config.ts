31bc48595cf2d96f3799790df25e61c36253a4cf
NHKhanq <nhkhang061203@gmail.com>
2025-04-16 14:37:49 +0700

    setup jest

---
+++ server/notification-service/jest.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  collectCoverage: true,
+  testPathIgnorePatterns: ["/node_modules/"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testMatch: ["<rootDir>/src/**/test/**/*.ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/test/**",
+    "!**/node_modules/**",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 1,
+      functions: 1,
+      lines: 1,
+      statements: 1,
+    },
+  },
+  coverageReporters: ["text-summary", "lcov"],
+  moduleNameMapper: {
+    "@notification/(.*)": ["<rootDir>/src/$1"],
+  },
+};
+
+export default config;
